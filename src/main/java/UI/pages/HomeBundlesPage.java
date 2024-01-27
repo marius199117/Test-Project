2fb28209e92e1d72595eb391ec4553b6e1260f36
Cuibus Marius <marius199117@gmail.com>
2024-01-27 02:18:31 +0200

    code aranngement

--- src/main/java/UI/pages/HomeBundlesPage.java
+++ src/main/java/UI/pages/HomeBundlesPage.java
@@ -6,15 +6,11 @@ import org.openqa.selenium.By;
 public class HomeBundlesPage extends CommonDefinitions {
     private final By bundlesButton = By.xpath("//div[@data-bdd='farefinder-option-bundles']");
     private final By carButton = By.xpath("//button[@data-bdd='farefinder-package-bundleoption-car']");
-
     private final By flyFrom = By.xpath("(//input[@placeholder='Enter address, city or airport'])[1]");
     private final By flyTo = By.xpath("(//input[@placeholder='Enter address, city or airport'])[2]");
-
     private final By departureDateLocator = By.xpath("//div[@class='picker__overlay picker__overlay--selectable'][@data-bdd='farefinder-package-startdate-input']");
-
     private final By startDateLocator = By.xpath("//td[@aria-label='February 12, 2024']");
     private final By endDateLocator = By.xpath("//td[@aria-label='February 24, 2024']");
-
     private final By departingTime = By.xpath("//select[@name='carPickupTime']");
     private final By returningTime = By.xpath("//select[@name='carDropoffTime']");
     private final By findADeal = By.xpath("//button[@type='submit']");
